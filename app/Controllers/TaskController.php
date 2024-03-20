67cf9017e0fd86d76deab956c16803989b0d38c4
Bamidele Sunday <sunday.bamidele@gmail.com>
2024-03-20 16:18:09 +0000

    Controller

---
+++ app/Controllers/TaskController.php
@@ -0,0 +1,19 @@
+//Controller for tasks (TaskController.php)
+
+class TaskController extends CI_Controller {
+    public function index() {
+        // Retrieve tasks from the database
+    }
+
+    public function store() {
+        // Create a new task
+    }
+
+    public function update($id) {
+        // Update an existing task
+    }
+
+    public function destroy($id) {
+        // Delete a task
+    }
+}
